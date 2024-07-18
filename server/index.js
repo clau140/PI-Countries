fa35a6d8232c0988f8856d31910f82f80a684343
clau140 <clau.alberdi89@gmail.com>
2024-07-18 13:47:49 -0300

    update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT= process.env.DB_PORT || 3001;
+//const PORT= process.env.DB_PORT || 3001;
+const PORT= process.env.DB_PORT
+
 const loadDb = require('./src/loadDB/loadDB.js')
 
 // Syncing all the models at once.
